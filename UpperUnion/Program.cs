0347bae994c5ed256e20a0cf12c538e76060f3d3
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-31 11:06:57 +0100

    added the string to upper case function

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+namespace UpperUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Writeline("NO ARGUMENTS");
+            
+            }
+            else
+            {
+                string[] uppercase = new string[args.Length];
+                
+                for (int i = 0; i < args.Length; i++);
+                {
+                    uppercase[i] = args[i].ToUpper();
+                }
+                Array.Sort(uppercase, StringComparer.OrdinalIgnoreCase);
+                
+            }
+        }
+    }
+}
