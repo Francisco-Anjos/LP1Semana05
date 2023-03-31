818cd380b6f90ced803b5eb836367b8548558161
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-31 10:36:11 +0100

    added the dice roll mechanic

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -6,7 +6,21 @@ namespace RandomDice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Would you kindly give me a number of dices");
+            int d = int.Parse(Console.Readline());
+
+            int ad = 0;
+
+            Random dice = new Random();
+
+            for (int i = 1;i <= d; i++);
+            {
+                int roll = dice.Next(1,7);
+                ad += roll;
+                Console.WriteLine($"You have rolled a {roll}");
+
+            }
+            Console.Writeline($"The final addition of all {d} rolls is {ad}");
         }
     }
 }
